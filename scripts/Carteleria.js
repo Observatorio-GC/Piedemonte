71a9099cc31e1c2474c634f2ed0dd8700d23fe8a
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2022-03-21 11:29:52 -0300

    Add files via upload

---
+++ scripts/Carteleria.js
@@ -0,0 +1,12 @@
+var Carteleria ={
+"type": "FeatureCollection",
+"name": "Carteleria",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ -68.879603754286748, -32.926070497888546 ] } },
+{ "type": "Feature", "properties": { "id": 2 }, "geometry": { "type": "Point", "coordinates": [ -68.897396727984912, -32.93374374107492 ] } },
+{ "type": "Feature", "properties": { "id": 3 }, "geometry": { "type": "Point", "coordinates": [ -68.95336715225497, -32.927104204975215 ] } },
+{ "type": "Feature", "properties": { "id": 4 }, "geometry": { "type": "Point", "coordinates": [ -68.916889667323019, -32.932101342526856 ] } },
+{ "type": "Feature", "properties": { "id": 5 }, "geometry": { "type": "Point", "coordinates": [ -68.933602218414464, -32.930059399105275 ] } }
+]
+}
---
+++ scripts/Garita.js
@@ -0,0 +1,8 @@
+var Garita ={
+"type": "FeatureCollection",
+"name": "Garita",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ -68.906028775734484, -32.932767183451602 ] } }
+]
+}
