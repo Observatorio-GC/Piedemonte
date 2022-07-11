646e875723e523fc8382ae022295417dd6cbc5bd
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2022-07-11 10:07:37 -0300

    Add files via upload

---
+++ scripts/Control.js
@@ -0,0 +1,8 @@
+var Control ={
+"type": "FeatureCollection",
+"name": "Control",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "Control Policial" }, "geometry": { "type": "Point", "coordinates": [ -68.909770503636722, -32.932087244265794 ] } }
+]
+}
