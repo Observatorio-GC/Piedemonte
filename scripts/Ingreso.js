007e724de0311a86e3a3e4006d9a17f3a197c617
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2022-07-11 09:59:38 -0300

    Add files via upload

---
+++ scripts/Ingreso.js
@@ -0,0 +1,8 @@
+var Ingreso ={
+"type": "FeatureCollection",
+"name": "Ingreso",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ -68.947139826741505, -32.928554028878544 ] } }
+]
+}
